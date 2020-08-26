db65c80f47165be5dd6d3b199a01b5eb1a56fec4
Виталий Лукин <mr.lukin1990@mail.ru>
2020-08-26 21:27:08 +0900

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speedx = 14f;
+    public float speedy = 5f;
+    private Rigidbody2D rbb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rbb.velocity = new Vector2(0f, speedy);
+        }
+    }
+}
